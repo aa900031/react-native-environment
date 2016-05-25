adb30229b1f3fc5198ab7e160ea22490ccce51c2
zhong666 <aa900031@gmail.com>
2016-05-25 12:40:49 +0800

    init

---
+++ ios/RNEnvironment/RNEnvironment/RNEnvironment.h
@@ -0,0 +1,13 @@
+//
+//  RNEnvironment.h
+//  RNEnvironment
+//
+//  Created by zhong666 on 2016/5/25.
+//  Copyright © 2016年 github. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RNEnvironment : NSObject
+
+@end
---
+++ ios/RNEnvironment/RNEnvironment/RNEnvironment.m
@@ -0,0 +1,13 @@
+//
+//  RNEnvironment.m
+//  RNEnvironment
+//
+//  Created by zhong666 on 2016/5/25.
+//  Copyright © 2016年 github. All rights reserved.
+//
+
+#import "RNEnvironment.h"
+
+@implementation RNEnvironment
+
+@end
